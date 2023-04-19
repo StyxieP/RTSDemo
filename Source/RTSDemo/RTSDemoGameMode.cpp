1ddc48d7391235393332f3c6d7c0e92b16697d1e
mattAlt <78375460+altMatt@users.noreply.github.com>
2023-04-19 21:52:52 +0100

    cleaning up GameMode.cpp

--- Source/RTSDemo/RTSDemoGameMode.cpp
+++ Source/RTSDemo/RTSDemoGameMode.cpp
@@ -2,7 +2,6 @@
 
 #include "RTSDemoGameMode.h"
 #include "RTSDemoPlayerController.h"
-#include "BaseCamera.h"
 #include "UObject/ConstructorHelpers.h"
 
 ARTSDemoGameMode::ARTSDemoGameMode()
@@ -10,10 +9,10 @@ ARTSDemoGameMode::ARTSDemoGameMode()
 	// use our custom PlayerController class
 	PlayerControllerClass = ARTSDemoPlayerController::StaticClass();
 	
-	if (DefaultPawnClass == ADefaultPawn::StaticClass())
-	{
-		DefaultPawnClass = ABaseCamera::StaticClass();
-	}
+	//if (DefaultPawnClass == ADefaultPawn::StaticClass())
+	//{
+	//	DefaultPawnClass = ABaseCamera::StaticClass();
+	//}
 
 	// set default pawn class to our Blueprinted character
 	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Script/RTSDemo.RTSDemoCharacter"));
